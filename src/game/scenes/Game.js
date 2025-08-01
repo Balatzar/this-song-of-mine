950a487865a4adc100da2d8fb1ffa58ea691be7d
Blatalzar <bataprod@gmail.com>
2025-08-01 22:26:51 +0200

    Adjust level

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -67,7 +67,7 @@ export class Game extends Scene {
 
         new ExitSign(this, 30, 1, 0, 0, this.player);
 
-        this.createBlockAt(6, 2, "dirt_block", -20, -20);
+        this.createBlockAt(6, 2, "dirt_block", -30, -20);
         this.createBlockAt(11, 1, "dirt_block", -20, -20);
         this.createBlockAt(11, 2, "dirt_block", -20, -20);
         this.createBlockAt(12, 2, "dirt_block", -20, -20);
@@ -75,7 +75,7 @@ export class Game extends Scene {
 
         new OneWayPlatform(this, 7, 2, -20, 10, this.player);
 
-        new Bridge(this, 8, 2, 0, 3, this.player);
+        new Bridge(this, 8, 2, -10, 3, this.player);
         new Bridge(this, 9, 2, -20, 3, this.player);
         new Bridge(this, 10, 2, -20, 3, this.player);
 
